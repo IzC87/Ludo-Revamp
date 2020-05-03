425fc834f112996742fb71069d3bb3440c2e662b
Fredrik Odén <fredrikoden87@gmail.com>
2020-05-03 08:49:44 +0200

    Updated databse to remove unused column

--- GameEngine/Classes/Token.cs
+++ GameEngine/Classes/Token.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Windows.Shapes;
 
 namespace GameEngine.Classes
---
+++ GameEngine/Migrations/20200503064809_RemovedNumberOfPlayersColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameEngine.Migrations
+{
+    public partial class RemovedNumberOfPlayersColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfPlayers",
+                table: "Games");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfPlayers",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
